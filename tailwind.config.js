796b92e3ee269bac91dc18e6f3dbabd4e4e347b0
RedTechX <jeremy.fangyc@gmail.com>
2024-11-12 18:24:08 +0800

    <div></div>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["retro", "valentine", "lemonade"],
+    themes: [
+      {
+        oceanMonument: {
+          primary: "#93c5fd",
+
+          secondary: "#a5f3fc",
+
+          accent: "#1d4ed8",
+
+          neutral: "#0891b2",
+
+          "base-100": "#0c4a6e",
+
+          info: "#818cf8",
+
+          success: "#a7f3d0",
+
+          warning: "#fef08a",
+
+          error: "#fecaca",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
